26261fb7316bb18066b25b2d629abfd940dc10e6
Vincent Kyalo <vincentkyalo@microsoft.com>
2022-05-05 09:01:52 +0300

    Initial commit

---
+++ JackAnalyzer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JackAnalyzer
+{
+    internal class JackAnalyzer
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
